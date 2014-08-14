aa96b783b5701643a8e33b1141ce23c317425757
He,Frank <frank.he@spirent.com>
2014-08-14 08:21:25 -0700

    add detail

--- public/js/views/InvitationView.js
+++ public/js/views/InvitationView.js
@@ -2,6 +2,7 @@ define(function(require){
 	
 	var Backbone 		= require('backbone'),
 		invitation_detail_tpl	= require('text!tpl/invitation_detail.html'),
+		invitation_detail_detail_tpl	= require('text!tpl/invitation_detail_detail.html'),
 		HeaderView = require('views/HeaderView'),
 		FooterView = require('views/FooterView'),
 		InvitationModel = require('models/invitationModel')
@@ -15,30 +16,40 @@ define(function(require){
         initialize: function() {
         	 this.template = _.template( invitation_detail_tpl );
         	 this.invitation = new InvitationModel.Invitation();
-        	 this.invitation.on('reset', this.update);
         },
         
         setId: function(id){
-        	this.invitation.getInvitationById(id);
+        	this.id = id;
         },
         
-        events:{
-        	
-        },
-        
-        update: function(){
-        	console.log("updated");
-        },
-       
-        render: function() {           
+        render: function() { 
             $(this.el).html(this.template({ user: util.getLoggedInUser() }));
             new HeaderView({ el: $(".headerContent", this.el)}).setTitle("Invitation").render();
             new FooterView({ el: $(".footerContent", this.el)}).render();
-            //this.invitationCollection.getMyPendingInvitations();
-            
+            var detailView = new InvitationDetailDetailView({ model: this.invitation});
+            detailView.setId(this.id);
             return this;
         }
     } );
+    
+    var InvitationDetailDetailView = Backbone.View.extend( {
+    	 initialize: function() {
+         	 this.model.on('reset', this.render);
+         },
+         
+         setId: function(id){
+        	 var _self = this;
+        	 setTimeout( function(){
+        		 _self.model.getInvitationById(id);
+        	 }, 1 );
+         },
+         
+         render: function(){
+        	 this.template = _.template( invitation_detail_detail_tpl );
+        	 $("#divInvDetail").html( "this is an error content" );
+        	 
+         }
+    });
     
     return InvitationView;
    
