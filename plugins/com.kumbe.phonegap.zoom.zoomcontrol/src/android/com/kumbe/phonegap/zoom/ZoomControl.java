49de72461090706959d22c463b0e313ba60299b6
He,Frank <frank.he@spirent.com>
2015-06-07 19:55:57 -0700

    add contact us link

---
+++ plugins/com.kumbe.phonegap.zoom.zoomcontrol/src/android/com/kumbe/phonegap/zoom/ZoomControl.java
@@ -0,0 +1,89 @@
+package com.kumbe.phonegap.zoom;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.webkit.WebSettings.ZoomDensity;
+import android.webkit.WebView;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class ZoomControl extends CordovaPlugin {
+	@Override
+	public boolean execute(String action, final JSONArray args,
+			final CallbackContext callbackContext) throws JSONException {
+		
+        if ("ZoomControl".equals(action)) {
+        	        	
+        	final WebView webView = this.webView;
+        	
+            cordova.getActivity().runOnUiThread(new Runnable() {
+
+				public void run() {
+
+                	try {
+
+                		boolean enabled=args.getBoolean(0);
+                		
+            			LOG.d("SetZoomControl", "executing SetZoomControl");
+
+            			webView.getSettings().setBuiltInZoomControls(enabled);
+            			webView.getSettings().setDefaultZoom(ZoomDensity.MEDIUM);
+            			webView.getSettings().setSupportZoom(enabled);
+            			callbackContext.success("OK");
+
+            		} catch (Exception e) {
+            			LOG.e("SetZoomControl", "Error: " + e.getMessage());
+            			callbackContext.error("Error: " + e.getMessage());
+
+            		}
+            		
+                }
+            });
+            return true;
+        }
+
+        if ("setBuiltInZoomControls".equals(action)) {
+            final WebView webView = this.webView;
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    try {
+                        boolean enabled=args.getBoolean(0);
+                        LOG.d("setBuiltInZoomControls", "executing setBuiltInZoomControls");
+                        webView.getSettings().setBuiltInZoomControls(enabled);
+                        callbackContext.success("OK");
+                    } catch (Exception e) {
+                        LOG.e("setBuiltInZoomControls", "Error: " + e.getMessage());
+                        callbackContext.error("Error: " + e.getMessage());
+                    }
+                }
+            });
+            return true;
+        }
+
+        if ("setDisplayZoomControls".equals(action)) {
+            final WebView webView = this.webView;
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    try {
+                        boolean enabled=args.getBoolean(0);
+                        LOG.d("setDisplayZoomControls", "executing setDisplayZoomControls");
+                        webView.getSettings().setDisplayZoomControls(enabled);
+                        callbackContext.success("OK");
+                    } catch (Exception e) {
+                        LOG.e("setDisplayZoomControls", "Error: " + e.getMessage());
+                        callbackContext.error("Error: " + e.getMessage());
+                    }
+                }
+            });
+            return true;
+        }
+
+        return false;
+        
+	}
+}
